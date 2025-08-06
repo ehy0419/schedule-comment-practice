6ed16291bf5fb2a569f15b6c7b202c61fb819043
ehy0419 <2hy0419@naver.com>
2025-08-06 20:21:01 +0900

    feat. SaveResponseDto 설정

---
+++ src/main/java/com/schedulecommentpractice/dto/ScheduleSaveResponse.java
@@ -0,0 +1,29 @@
+package com.schedulecommentpractice.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ScheduleSaveResponse {     /// API 응답에서는 비밀번호는 제외
+
+    /// ResponseDto 에서는 한 번 객체가 만들어지면 필드를 바꿀 일은 거의 없다.
+    /// 그래서 필드에 final을 붙여서 상수를 붙일 수 있다.
+
+    private final Long id;        // 고유 식별자
+    private final String title;
+    private final String content;
+    private final String author;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+    //    private String password;        // API 응답에서는 비밀번호는 제외
+
+    public ScheduleSaveResponse(Long id, String title, String content, String author, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+}
--- src/main/java/com/schedulecommentpractice/entity/Schedule.java
+++ src/main/java/com/schedulecommentpractice/entity/Schedule.java
@@ -7,16 +7,22 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Getter
 @NoArgsConstructor
-public class Schedule {
+public class Schedule extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;        // 고유 식별자
     private String title;
     private String content;
     private String author;
     private String password;
+
+    ///  BaseEntitiy 에서 상속을 받아서 사용하는 부분
+    // 작성일과 수정일 완성.
+//    private LocalDateTime createdAt;
+//    private LocalDateTime modifiedAt;
 }
